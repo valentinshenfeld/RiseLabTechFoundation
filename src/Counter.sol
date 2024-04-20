7e042ad81006e48d073d7e6993ee72f2e04982e0
valentinshenfeld <shenfeld.valentin@gmail.com>
2024-04-20 13:09:12 +0300

    Add constructor in smart contract

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    address owner;
+
+    constructor(address _owner){
+        owner=_owner;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
